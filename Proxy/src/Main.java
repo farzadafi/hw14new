733db904dee3b85886025b5ff4861f6dfff818db
farzad <farzadafi40@gmail.com>
2022-04-02 17:20:11 +0430

    create Main class for run and test program

---
+++ Proxy/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ExpensiveObject object = new ExpensiveObjectProxy();
+        object.process();
+        object.process();
+    }
+}
