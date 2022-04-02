f8fe345391dcb361c7899d59a864b06d5376c096
farzad <farzadafi40@gmail.com>
2022-04-02 18:00:51 +0430

    create SessionFactorySingleton and add some method for give just a connection to datebase

---
+++ Twitter/src/main/java/connection/SessionFactorySingleton.java
@@ -0,0 +1,34 @@
+package connection;
+
+import entity.Comment;
+import entity.Twit;
+import entity.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactorySingleton {
+
+    private SessionFactorySingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(Twit.class)
+                    .addAnnotatedClass(Comment.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
