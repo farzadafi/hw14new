9d5a26e55e471b5327dfb9f44e51af4e9faa0743
farzad <farzadafi40@gmail.com>
2022-04-02 21:29:48 +0430

    add findByUserName method

--- Twitter/src/main/java/service/impel/UserServiceImpel.java
+++ Twitter/src/main/java/service/impel/UserServiceImpel.java
@@ -1,8 +1,26 @@
 package service.impel;
 
+import connection.SessionFactorySingleton;
 import entity.User;
+import org.hibernate.SessionFactory;
+import repository.impel.UserRepositoryImpel;
 import service.UserService;
 
 public class UserServiceImpel extends GenericServiceImpel<User,Integer> implements UserService {
 
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    private final UserRepositoryImpel userRepositoryImpel = new UserRepositoryImpel();
+
+    @Override
+    public User findByUserName(String username) {
+        try (var session = sessionFactory.getCurrentSession()) {
+            session.getTransaction().begin();
+            try {
+                return userRepositoryImpel.findByUserName(username);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+        }
+    }
 }
