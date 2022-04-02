b9409d22d88df1701abfd294be943d4e22e97b74
farzad <farzadafi40@gmail.com>
2022-04-02 21:54:16 +0430

    add method add

--- Twitter/src/main/java/service/impel/UserServiceImpel.java
+++ Twitter/src/main/java/service/impel/UserServiceImpel.java
@@ -5,11 +5,27 @@ import entity.User;
 import org.hibernate.SessionFactory;
 import repository.impel.UserRepositoryImpel;
 import service.UserService;
+import utility.Utility;
 
 public class UserServiceImpel extends GenericServiceImpel<User,Integer> implements UserService {
 
     private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
     private final UserRepositoryImpel userRepositoryImpel = new UserRepositoryImpel();
+    private static final Utility utility = new Utility();
+
+    @Override
+    public User add(User userNull) {
+        String fullName = utility.setFullName();
+        String userName = utility.setUserName();
+        String password = utility.setPassword();
+        User user = new User(fullName,userName,password,null,null);
+        User userResult = super.add(user);
+        if(userResult != null)
+            System.out.println(fullName + " successful added!");
+        else
+            System.out.println("Something is wrong!");
+        return userResult;
+    }
 
     @Override
     public User findByUserName(String username) {
