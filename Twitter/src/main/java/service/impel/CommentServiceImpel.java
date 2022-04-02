ca13fa829b4b1c792a48c04731d80f34e5506c1c
farzad <farzadafi40@gmail.com>
2022-04-02 20:51:23 +0430

    create CommentServiceImpel and extends from GenericServiceImpel and implements from CommentService

---
+++ Twitter/src/main/java/service/impel/CommentServiceImpel.java
@@ -0,0 +1,8 @@
+package service.impel;
+
+import entity.Comment;
+import service.CommentService;
+
+public class CommentServiceImpel extends GenericServiceImpel<Comment,Integer> implements CommentService {
+
+}
