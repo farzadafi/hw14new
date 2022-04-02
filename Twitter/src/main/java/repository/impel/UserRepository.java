40ea45c22f342b5b24ef55bc3fca025a27860a1f
farzad <farzadafi40@gmail.com>
2022-04-02 21:30:04 +0430

    add findByUserName method

--- Twitter/src/main/java/repository/impel/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package repository.impel;
-
-import entity.User;
-
-public class UserRepository extends GenericRepositoryImpel<User,Integer> implements repository.UserRepository {
-}
--- Twitter/src/main/java/service/UserService.java
+++ Twitter/src/main/java/service/UserService.java
@@ -4,4 +4,5 @@ import entity.User;
 import service.baseService.BaseService;
 
 public interface UserService extends BaseService<User,Integer> {
+    User findByUserName(String username);
 }
