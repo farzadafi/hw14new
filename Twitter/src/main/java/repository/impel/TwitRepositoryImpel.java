6c650c5db1ce5bef1816b69b484ccc5971462100
farzad <farzadafi40@gmail.com>
2022-04-02 18:51:07 +0430

    create TwitRepositoryImpel and extends it from GenericRepositoryImpel and implements it from TwitRepository

---
+++ Twitter/src/main/java/repository/impel/TwitRepositoryImpel.java
@@ -0,0 +1,7 @@
+package repository.impel;
+
+import entity.Twit;
+import repository.TwitRepository;
+
+public class TwitRepositoryImpel extends GenericRepositoryImpel<Twit,Integer> implements TwitRepository {
+}
