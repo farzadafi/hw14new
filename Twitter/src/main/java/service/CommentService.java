6eb893602f10449feebf2b2c9fbf6ae2fdc66f18
farzad <farzadafi40@gmail.com>
2022-04-03 19:26:48 +0430

    add showComment,findCommentByTwitId,findWithParentId,replyComment

--- Twitter/src/main/java/service/CommentService.java
+++ Twitter/src/main/java/service/CommentService.java
@@ -1,7 +1,15 @@
 package service;
 
 import entity.Comment;
+import entity.Twit;
+import entity.User;
 import service.base.BaseService;
 
+import java.util.List;
+
 public interface CommentService extends BaseService<Comment,Integer> {
+    void showComment(Twit twit);
+    List<Comment> findCommentByTwitId(Twit twit);
+    List<Comment> findWithParentId(Integer id);
+    void replyComment(User user);
 }
