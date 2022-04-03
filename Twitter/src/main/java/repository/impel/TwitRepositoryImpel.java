7e8ee8c55b6210521281e1d5c59cc45e1e04416f
farzad <farzadafi40@gmail.com>
2022-04-03 16:35:10 +0430

    dd showTwitId and findTwitByTwoId method

--- Twitter/src/main/java/repository/impel/TwitRepositoryImpel.java
+++ Twitter/src/main/java/repository/impel/TwitRepositoryImpel.java
@@ -1,7 +1,32 @@
 package repository.impel;
 
+import connection.SessionFactorySingleton;
 import entity.Twit;
+import org.hibernate.SessionFactory;
 import repository.TwitRepository;
 
+import java.util.List;
+
 public class TwitRepositoryImpel extends GenericRepositoryImpel<Twit,Integer> implements TwitRepository {
+
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+
+    @Override
+    public List<Twit> showTwitById(Integer userId) {
+        var session = sessionFactory.getCurrentSession();
+        var query = session.createQuery("FROM Twit as t WHERE t.user.id = :user_id",Twit.class);
+        query.setParameter("user_id",userId);
+        return query.list();
+    }
+
+    @Override
+    public Twit findTwitByTwoId(Integer userId, Integer twitId) {
+        var session = sessionFactory.getCurrentSession();
+        var query = session.createQuery("FROM Twit as t WHERE t.id = :id AND" +
+                " t.user.id = :user_id",Twit.class);
+        query.setParameter("user_id",userId);
+        query.setParameter("id",twitId);
+        return query.getSingleResult();
+    }
+
 }
