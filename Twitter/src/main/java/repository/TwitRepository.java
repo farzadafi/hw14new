6c97c2d96f697aabb0a22cf178f0bd20afd53555
farzad <farzadafi40@gmail.com>
2022-04-03 16:35:17 +0430

    dd showTwitId and findTwitByTwoId method

--- Twitter/src/main/java/repository/TwitRepository.java
+++ Twitter/src/main/java/repository/TwitRepository.java
@@ -3,5 +3,9 @@ package repository;
 import entity.Twit;
 import repository.base.GenericRepository;
 
+import java.util.List;
+
 public interface TwitRepository extends GenericRepository<Twit,Integer> {
+    List<Twit> showTwitById(Integer userId);
+    Twit findTwitByTwoId(Integer userId,Integer twitId);
 }
