5f6e4e2363e2c588f2d5d2fb4cdda01d2bcb7163
farzad <farzadafi40@gmail.com>
2022-04-03 21:54:26 +0430

    add unfollow method

--- Twitter/src/main/java/service/FollowerSevice.java
+++ Twitter/src/main/java/service/FollowerSevice.java
@@ -1,4 +1,7 @@
 package service;
 
+import entity.User;
+
 public interface FollowerSevice {
+    Integer unFollow(User user1,User user2);
 }
--- Twitter/src/main/java/service/impel/FollowerServiceImpel.java
+++ Twitter/src/main/java/service/impel/FollowerServiceImpel.java
@@ -1,4 +1,24 @@
 package service.impel;
 
+import connection.SessionFactorySingleton;
+import entity.User;
+import org.hibernate.SessionFactory;
+import repository.impel.FollowerRepositoryImpel;
+
 public class FollowerServiceImpel {
+
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    private final FollowerRepositoryImpel followerRepositoryImpel = new FollowerRepositoryImpel();
+
+    public Integer unfollow(User user1, User user2){
+        try (var session = sessionFactory.getCurrentSession()) {
+            session.getTransaction().begin();
+            try {
+                return followerRepositoryImpel.unFollow(user1.getId(),user2.getId());
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+                return null;
+            }
+        }
+    }
 }
