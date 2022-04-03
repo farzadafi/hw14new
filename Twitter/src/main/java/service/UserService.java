b9fb643f8350e69240bb5dec11608fdf7a233ecc
farzad <farzadafi40@gmail.com>
2022-04-03 16:48:04 +0430

    add addTwit,deleteTwit,updateTwit method

--- Twitter/src/main/java/service/UserService.java
+++ Twitter/src/main/java/service/UserService.java
@@ -5,4 +5,7 @@ import service.base.BaseService;
 
 public interface UserService extends BaseService<User,Integer> {
     User findByUserName(String username);
+    void addTwit(User user);
+    void deleteTwit(User user);
+    void updateTwit(User user);
 }
