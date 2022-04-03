b86690fea997946844ae779ae9af0633e639f706
farzad <farzadafi40@gmail.com>
2022-04-03 20:31:53 +0430

    add findByUserId,findById method

--- Twitter/src/main/java/repository/CommentRepository.java
+++ Twitter/src/main/java/repository/CommentRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface CommentRepository extends GenericRepository<Comment,Integer> {
     List<Comment> showCommentByTwitId(Integer id);
     List<Comment> findByParentId(Integer id);
+    List<Comment> findByUserId(Integer id);
+    Comment findById(Integer id);
 }
