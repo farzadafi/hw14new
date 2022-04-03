fc51fb700bfacfc0e33332baeb1c568bff15b84d
farzad <farzadafi40@gmail.com>
2022-04-03 16:01:54 +0430

    create Base service interface

---
+++ Twitter/src/main/java/service/base/BaseService.java
@@ -0,0 +1,11 @@
+package service.base;
+
+import java.util.List;
+
+public interface BaseService<K,ID extends Integer> {
+    K add (K k);
+    K findById(ID id);
+    List<K> findAll();
+    void update(K t);
+    void delete(K k);
+}
