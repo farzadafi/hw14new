4a0bb612ae4e508d582a83b3bf34c05a16224e45
farzad <farzadafi40@gmail.com>
2022-04-03 19:24:20 +0430

    add showCommentTwitId,findByParentId method

--- Twitter/src/main/java/repository/CommentRepository.java
+++ Twitter/src/main/java/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package repository;
 import entity.Comment;
 import repository.base.GenericRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends GenericRepository<Comment,Integer> {
+    List<Comment> showCommentByTwitId(Integer id);
+    List<Comment> findByParentId(Integer id);
 }
