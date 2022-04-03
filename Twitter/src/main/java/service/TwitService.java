cea580205bc6022f9a8a6db31686f444563e557f
farzad <farzadafi40@gmail.com>
2022-04-03 16:47:12 +0430

    add showMyTwit,findTwitById,findTwitByTwoId method

--- Twitter/src/main/java/service/TwitService.java
+++ Twitter/src/main/java/service/TwitService.java
@@ -2,7 +2,13 @@ package service;
 
 
 import entity.Twit;
+import entity.User;
 import service.base.BaseService;
 
+import java.util.List;
+
 public interface TwitService extends BaseService<Twit,Integer> {
+    Integer showMyTwit(User user);
+    List<Twit> findTwitById(User user);
+    Twit findTwitByTwoId(User user,Integer TwitId);
 }
